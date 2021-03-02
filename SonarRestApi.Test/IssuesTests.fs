8922a349f60c737722782ee8bb393eeccfc07297
Jorge Costa <jorge.costa@trimble.com>
2021-03-02 11:59:18 +0200

    fix tests

--- SonarRestApi.Test/IssuesTests.fs
+++ SonarRestApi.Test/IssuesTests.fs
@@ -43,20 +43,6 @@ type IssuesTests() =
         Assert.That(issues.Count, Is.EqualTo(2))
 
     [<Test>]
-    member test.``Should Get Issues In Project by user using old format`` () =
-        let conf = ConnectionConfiguration("http://localhost:9000", "login1", "password", 4.5)
-
-        let mockHttpReq =
-            Mock<IHttpSonarConnector>()
-                .Setup(fun x -> <@ x.HttpSonarGetRequest(conf, "/api/reviews?projects=groupid:projectid&assignees=login1") @>).Returns(File.ReadAllText(assemblyRunningPath + "/testdata/reviewByUser.txt"))
-                .Setup(fun x -> <@ x.HttpSonarGetRequest(conf, "/api/issues/search?componentRoots=groupid:projectid&assignees=login1") @>).Returns(File.ReadAllText(assemblyRunningPath + "/testdata/NonExistentPage.xml"))
-                .Create()
-
-        let service = SonarService(mockHttpReq)
-        let issues = (service :> ISonarRestService).GetIssuesByAssigneeInProject(conf, "groupid:projectid", "login1", cancelMonitors.Token, mockLogger).GetAwaiter().GetResult()
-        Assert.That(issues.Count, Is.EqualTo(5))
-
-    [<Test>]
     member test.``Should Get All Issues In Project`` () =
         let conf = ConnectionConfiguration("http://localhost:9000", "jocs1", "jocs1", 4.5)
 
